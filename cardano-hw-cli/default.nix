788972caded5d63529c8b6c50526b55539d16906
Samuel Leathers <samuel.leathers@iohk.io>
2021-02-15 12:59:14 -0500

    cardano-hw-cli: bump to latest

--- cardano-hw-cli/default.nix
+++ cardano-hw-cli/default.nix
@@ -1,14 +1,14 @@
 { stdenv, lib, fetchurl, patchelf, libusb, libudev }:
 
 let
-  version = "1.1.1";
+  version = "1.2.0-rc0";
 in stdenv.mkDerivation {
   pname = "cardano-hw-cli";
   version = "v${version}";
   src = fetchurl {
     url = "https://github.com/vacuumlabs/cardano-hw-cli/releases/download/v${version}/cardano-hw-cli-${version}_linux-x64.tar.gz";
     #url = "https://s3-eu-central-1.amazonaws.com/ci-static/cardano-hw-cli.tar.gz";
-    sha256 = "sha256:09dzv6bdgc02xqjlbhwy1vypmmhb8y7bdpxhx32v7pi9c17nfymp";
+    sha256 = "sha256-fcpu/q0sNlO86elhphiCYJAuIWsduJtu0JrG1gdBK5g=";
   };
   phases = [ "unpackPhase" "installPhase" ];
   #sourceRoot = ".";
