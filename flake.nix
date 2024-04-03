4096243a50cd3bf238f19c4ef2b1925002d2e045
Samuel Leathers <samuel.leathers@iohk.io>
2024-04-03 16:13:58 -0400

    Use flake-utils to support cross system

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
   description = "adawallet";
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     cardano-parts.url = "github:input-output-hk/cardano-parts";
     nixpkgs.follows = "cardano-parts/nixpkgs";
   };
-  outputs = { self, nixpkgs, cardano-parts, ... }@inputs: let
+  outputs = { self, flake-utils, nixpkgs, cardano-parts, ... }@inputs:
+  flake-utils.lib.eachDefaultSystem (system: let
     overlay = import ./overlay.nix { inherit inputs self; };
     pkgs = import nixpkgs {
-      system = "x86_64-linux";
+      inherit system;
       overlays = [ overlay ];
     };
+
   in {
     inherit overlay;
-    legacyPackages.x86_64-linux = pkgs;
-    devShell.x86_64-linux = pkgs.devShell;
-  };
+    legacyPackages = pkgs;
+    devShell = pkgs.devShell;
+  });
 }
