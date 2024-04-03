49ed8d8b42fe94ab22b383ded87c6cb8449e7824
Samuel Leathers <samuel.leathers@iohk.io>
2024-04-03 16:31:39 -0400

    unsystemize overlay

--- flake.nix
+++ flake.nix
@@ -5,17 +5,18 @@
     cardano-parts.url = "github:input-output-hk/cardano-parts";
     nixpkgs.follows = "cardano-parts/nixpkgs";
   };
-  outputs = { self, flake-utils, nixpkgs, cardano-parts, ... }@inputs:
-  flake-utils.lib.eachDefaultSystem (system: let
-    overlay = import ./overlay.nix { inherit inputs self; };
+  outputs = { self, flake-utils, nixpkgs, cardano-parts, ... }@inputs: let
+  overlay = import ./overlay.nix { inherit inputs self; };
+  in flake-utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs {
       inherit system;
       overlays = [ overlay ];
     };
 
   in {
-    inherit overlay;
     legacyPackages = pkgs;
     devShell = pkgs.devShell;
-  });
+  }) // {
+    inherit overlay;
+  };
 }
