b64e7879df8460e2ca6c6297fe53151728c815f1
Samuel Leathers <samuel.leathers@iohk.io>
2024-04-03 15:51:37 -0400

    update to pull cardano binaries from cardano-parts
    uses content addressed packages so should build much quicker

--- flake.nix
+++ flake.nix
@@ -1,11 +1,10 @@
 {
   description = "adawallet";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    cardano-node.url = "github:input-output-hk/cardano-node/8.1.2";
-    cardano-addresses.url = "github:input-output-hk/cardano-addresses";
+    cardano-parts.url = "github:input-output-hk/cardano-parts";
+    nixpkgs.follows = "cardano-parts/nixpkgs";
   };
-  outputs = { self, nixpkgs, cardano-node, cardano-addresses, ... }@inputs: let
+  outputs = { self, nixpkgs, cardano-parts, ... }@inputs: let
     overlay = import ./overlay.nix { inherit inputs self; };
     pkgs = import nixpkgs {
       system = "x86_64-linux";
