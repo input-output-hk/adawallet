fbaf86ab54b0cc451609a1f0f2faf97f3ed10083
Samuel Leathers <samuel.leathers@iohk.io>
2024-05-10 10:43:01 -0400

    initial commit

---
+++ adawallet/adawallet.hs
@@ -0,0 +1,7 @@
+module Main (main) where
+
+import qualified Adawallet
+import Prelude
+
+main :: IO ()
+main = Adawallet.main
---
+++ adawallet/src/Adawallet.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TupleSections #-}
+
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{- HLINT ignore "Use let" -}
+
+module Adawallet (main) where
+
+import Prelude
+
+main :: IO ()
+main = putStrLn "Hello World"
