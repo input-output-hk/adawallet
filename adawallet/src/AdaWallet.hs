eebe857ea869e57795c1b26a047691868bbcc890
Michael Fellinger <michael.fellinger@iohk.io>
2024-06-08 16:52:56 +0200

    add wipe command

--- adawallet/src/AdaWallet.hs
+++ adawallet/src/AdaWallet.hs
@@ -1,24 +1,35 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {- HLINT ignore "Use let" -}
 
 module AdaWallet (main) where
 
+import Control.Monad
+import Options.Applicative
+import System.Directory
 import Prelude
 
 main :: IO ()
-main = putStrLn "Hello World"
+main = join $ execParser (info opts idm)
 
 foo :: IO ()
 foo = putStrLn "Foo"
+
+data CLI = CLI
+  { quiet :: Bool
+  }
+
+opts :: Parser (IO ())
+opts =
+  hsubparser
+    ( command "wipe" (info (pure wipeCommand) (progDesc "wipe all state"))
+    )
+
+wipeCommand :: IO ()
+wipeCommand = do
+  dir <- stateDir
+  System.Directory.removeDirectoryRecursive dir
+
+stateDir :: IO FilePath
+stateDir =
+  System.Directory.getXdgDirectory System.Directory.XdgData "adawallet"
