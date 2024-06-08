8986bc12995928f40dad67108dca424d963c2752
Michael Fellinger <michael.fellinger@iohk.io>
2024-06-08 17:20:41 +0200

    handle ADAWALLET_* variables

--- adawallet/src/AdaWallet.hs
+++ adawallet/src/AdaWallet.hs
@@ -7,6 +7,7 @@ module AdaWallet (main) where
 import Control.Monad
 import Options.Applicative
 import System.Directory
+import System.Environment
 import Prelude
 
 main :: IO ()
@@ -31,5 +32,16 @@ wipeCommand = do
   System.Directory.removeDirectoryRecursive dir
 
 stateDir :: IO FilePath
-stateDir =
-  System.Directory.getXdgDirectory System.Directory.XdgData "adawallet"
+stateDir = do
+  fromEnv <- lookupEnv "ADAWALLET_STATE"
+  let fromXdg = System.Directory.getXdgDirectory System.Directory.XdgData "adawallet"
+  case fromEnv of
+    Nothing -> fromXdg
+    Just dir -> pure dir
+
+walletName :: IO String
+walletName = do
+  fromEnv <- lookupEnv "ADAWALLET_NAME"
+  case fromEnv of
+    Nothing -> pure "default"
+    Just name -> pure name
