74e482f5558b43ae0fbb3576a4fbf33a31a04b93
Samuel Leathers <samuel.leathers@iohk.io>
2024-06-09 09:13:01 -0400

    fourmolu changes

--- adawallet/src/Mnemonic/Conversion.hs
+++ adawallet/src/Mnemonic/Conversion.hs
@@ -2,50 +2,51 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 
-{-|
-This module is concerned with converting a mnemonic to 
-cardano-cli compatible verification and signing keys
--}
-module Mnemonic.Conversion where 
+-- |
+-- This module is concerned with converting a mnemonic to
+-- cardano-cli compatible verification and signing keys
+module Mnemonic.Conversion where
 
-import Prelude 
+import Prelude
 
 import Cardano.Address.Derivation
-import Cardano.Mnemonic
 import qualified Cardano.Codec.Bech32.Prefixes as CIP5
-import qualified Codec.Binary.Bech32 as Bech32
+import Cardano.Mnemonic
 import Codec.Binary.Bech32 hiding (encode)
+import qualified Codec.Binary.Bech32 as Bech32
 import Codec.Binary.Encoding
 import Control.Exception
 import Control.Monad
 import Control.Monad.Trans.Except.Extra
 import Control.Monad.Trans.Fail
 import Data.Bifunctor
+import Data.ByteString (ByteString)
 import Data.Functor.Identity
 import Data.Text (Text)
 import qualified Data.Text.Encoding as Text
-import Data.ByteString (ByteString)
 import GHC.TypeLits
 import GHC.Word
-import Options.Applicative.Style
 import Options.Applicative.Derivation
+import Options.Applicative.Style
 
 data RootExtendedKeyGenerationError
   = MnemonicError (MkSomeMnemonicError '[24])
   | RootKeyGenerationError IOException
 
-nemonicToRootExtendedPrivateKey 
-  :: [Text] -> IO (Either RootExtendedKeyGenerationError ByteString) 
-nemonicToRootExtendedPrivateKey mnemonic = runExceptT $ do 
-  -- TODO: Support password protected mnemonics 
+nemonicToRootExtendedPrivateKey ::
+  [Text] -> IO (Either RootExtendedKeyGenerationError ByteString)
+nemonicToRootExtendedPrivateKey mnemonic = runExceptT $ do
+  -- TODO: Support password protected mnemonics
   -- TODO: Potentially support Byron, Icarus and Shared mnemonics
   -- TODO: Potentially support other sizes of mnemonics (9, 12, 15, 18, 21)
-  let password = Nothing 
+  let password = Nothing
       style = Shelley
-  someMnemonic <- hoistEither . first MnemonicError 
-                    $ mkSomeMnemonic mnemonic
-  rootK <- handleIOExceptT RootKeyGenerationError  
-             $ generateRootKey someMnemonic password style
+  someMnemonic <-
+    hoistEither . first MnemonicError $
+      mkSomeMnemonic mnemonic
+  rootK <-
+    handleIOExceptT RootKeyGenerationError $
+      generateRootKey someMnemonic password style
   return $ encode (EBech32 $ styleHrp style) (xprvToBytes rootK)
 
 -- TODO: Export from cardano-address
@@ -53,34 +54,36 @@ styleHrp :: Style -> HumanReadablePart
 styleHrp Shared = CIP5.root_shared_xsk
 styleHrp _ = CIP5.root_xsk
 
-data PrivateExtendedKeyError 
+data PrivateExtendedKeyError
   = DataPartDecodeError
-  | Bech32DecodeError Bech32.DecodingError 
+  | Bech32DecodeError Bech32.DecodingError
   | DerivationPathError String
   | XPrvDecodeError ByteString
   | ChildKeyBech32HumuanReadablePartError String
 
--- | Convert the root extended private key to an extended private key. 
+-- | Convert the root extended private key to an extended private key.
 -- https://github.com/uniVocity/cardano-tutorials/blob/master/cardano-addresses.md#understanding-the-hd-wallet-address-format-bip-44
 rootExtendedPrivateKeyToPrivateKey :: ByteString -> Either PrivateExtendedKeyError ByteString
-rootExtendedPrivateKeyToPrivateKey rootExtendedPrivateKeyBytes = do 
-    (hrp, dataPart) <- first Bech32DecodeError . Bech32.decodeLenient 
-                         $ Text.decodeUtf8 rootExtendedPrivateKeyBytes
-    bytes <- maybe (Left DataPartDecodeError) Right $ Bech32.dataPartToBytes dataPart
-    xprv <- maybe (Left $ XPrvDecodeError bytes) Right $ xprvFromBytes bytes
-    let defaultDerivationPath = "1852H/1815H/0H/0/0"
-    derivPath <- first DerivationPathError $ derivationPathFromString defaultDerivationPath
-    
-    let ixs = castDerivationPath derivPath
-        scheme = if length ixs == 2 && hrp == CIP5.root_xsk
-                 then DerivationScheme1
-                 else DerivationScheme2
-    let Identity child = foldM (\k -> pure . deriveXPrv scheme k) xprv ixs
-    (hrp, child) <- 
-      (,xprvToBytes child) 
-        <$> first ChildKeyBech32HumuanReadablePartError (runFail $ childHrpFor (indexToWord32 <$> ixs) hrp)
-    return $ encode (EBech32 hrp) child
- where 
+rootExtendedPrivateKeyToPrivateKey rootExtendedPrivateKeyBytes = do
+  (hrp, dataPart) <-
+    first Bech32DecodeError . Bech32.decodeLenient $
+      Text.decodeUtf8 rootExtendedPrivateKeyBytes
+  bytes <- maybe (Left DataPartDecodeError) Right $ Bech32.dataPartToBytes dataPart
+  xprv <- maybe (Left $ XPrvDecodeError bytes) Right $ xprvFromBytes bytes
+  let defaultDerivationPath = "1852H/1815H/0H/0/0"
+  derivPath <- first DerivationPathError $ derivationPathFromString defaultDerivationPath
+
+  let ixs = castDerivationPath derivPath
+      scheme =
+        if length ixs == 2 && hrp == CIP5.root_xsk
+          then DerivationScheme1
+          else DerivationScheme2
+  let Identity child = foldM (\k -> pure . deriveXPrv scheme k) xprv ixs
+  (hrp, child) <-
+    (,xprvToBytes child)
+      <$> first ChildKeyBech32HumuanReadablePartError (runFail $ childHrpFor (indexToWord32 <$> ixs) hrp)
+  return $ encode (EBech32 hrp) child
+  where
     -- NB: We wholesale copy this from cardano-address
     -- As a reminder, we really have two scenarios:
     --
@@ -142,61 +145,55 @@ rootExtendedPrivateKeyToPrivateKey rootExtendedPrivateKeyBytes = do
     -- There's no use-case at the moment for accessing intermediate paths such
     -- as m / purpose' or m / purpose' / coin_type' so we do not expose them.
     childHrpFor :: [Word32] -> HumanReadablePart -> Fail String HumanReadablePart
-    childHrpFor [_,_,_,2,_] hrp
-        | hrp == CIP5.root_xsk = pure CIP5.stake_xsk
-        | hrp == CIP5.root_shared_xsk = pure CIP5.stake_shared_xsk
+    childHrpFor [_, _, _, 2, _] hrp
+      | hrp == CIP5.root_xsk = pure CIP5.stake_xsk
+      | hrp == CIP5.root_shared_xsk = pure CIP5.stake_shared_xsk
+    childHrpFor [p, _, _, _, _] hrp
+      | hrp == CIP5.root_xsk =
+          -- 2147485502 stands for 1854H
+          if p == 2147485502
+            then pure CIP5.addr_shared_xsk
+            else pure CIP5.addr_xsk
+      | hrp == CIP5.root_shared_xsk = pure CIP5.addr_shared_xsk
+    childHrpFor [p, _, _] hrp
+      | hrp == CIP5.root_xsk =
+          -- 2147485502 stands for 1854H
+          if p == 2147485502
+            then pure CIP5.acct_shared_xsk
+            else -- 2147485503 stands for 1855H
 
-    childHrpFor [p,_,_,_,_] hrp
-        | hrp == CIP5.root_xsk =
-              -- 2147485502 stands for 1854H
-              if p == 2147485502 then
-                  pure CIP5.addr_shared_xsk
-              else
-                  pure CIP5.addr_xsk
-        | hrp == CIP5.root_shared_xsk = pure CIP5.addr_shared_xsk
-
-    childHrpFor [p,_,_] hrp
-        | hrp == CIP5.root_xsk =
-              -- 2147485502 stands for 1854H
-              if p == 2147485502 then
-                  pure CIP5.acct_shared_xsk
-              -- 2147485503 stands for 1855H
-              else if p == 2147485503 then
-                  pure CIP5.policy_xsk
-              else
-                  pure CIP5.acct_xsk
-        | hrp == CIP5.root_shared_xsk = pure CIP5.acct_shared_xsk
-
-    childHrpFor [2,_] hrp
-        | hrp == CIP5.acct_xsk = pure CIP5.stake_xsk
-        | hrp == CIP5.acct_xvk = pure CIP5.stake_xvk
-        | hrp == CIP5.acct_shared_xsk = pure CIP5.stake_shared_xsk
-        | hrp == CIP5.acct_shared_xvk = pure CIP5.stake_shared_xvk
-
-    childHrpFor [_,_] hrp
-        | hrp == CIP5.root_xsk = pure CIP5.addr_xsk
-        | hrp == CIP5.acct_xsk = pure CIP5.addr_xsk
-        | hrp == CIP5.acct_xvk = pure CIP5.addr_xvk
-        | hrp == CIP5.acct_shared_xsk = pure CIP5.addr_shared_xsk
-        | hrp == CIP5.acct_shared_xvk = pure CIP5.addr_shared_xvk
-
+              if p == 2147485503
+                then pure CIP5.policy_xsk
+                else pure CIP5.acct_xsk
+      | hrp == CIP5.root_shared_xsk = pure CIP5.acct_shared_xsk
+    childHrpFor [2, _] hrp
+      | hrp == CIP5.acct_xsk = pure CIP5.stake_xsk
+      | hrp == CIP5.acct_xvk = pure CIP5.stake_xvk
+      | hrp == CIP5.acct_shared_xsk = pure CIP5.stake_shared_xsk
+      | hrp == CIP5.acct_shared_xvk = pure CIP5.stake_shared_xvk
+    childHrpFor [_, _] hrp
+      | hrp == CIP5.root_xsk = pure CIP5.addr_xsk
+      | hrp == CIP5.acct_xsk = pure CIP5.addr_xsk
+      | hrp == CIP5.acct_xvk = pure CIP5.addr_xvk
+      | hrp == CIP5.acct_shared_xsk = pure CIP5.addr_shared_xsk
+      | hrp == CIP5.acct_shared_xvk = pure CIP5.addr_shared_xvk
     childHrpFor _ hrp
-        | hrp == CIP5.root_xsk = fail
+      | hrp == CIP5.root_xsk =
+          fail
             "When deriving child keys from a parent root key, you must \
             \provide either 2, 3 or 5 path segments. Provide 2 (account and \
             \address) if you intend to derive a legacy Byron key. Provide 3 or 5 \
             \(purpose, coin_type, account, role, index) if you're dealing with \
             \anything else."
-
-        | hrp == CIP5.root_shared_xsk = fail
+      | hrp == CIP5.root_shared_xsk =
+          fail
             "When deriving child keys from a parent root key, you must \
             \provide either 3 or 5 path segments. Provide 3 \
             \(purpose, coin_type, account) or 5 \
             \(purpose, coin_type, account, role, index)."
-
-        | otherwise = fail
+      | otherwise =
+          fail
             "When deriving child keys from a parent account key, you must \
             \provide exactly two path segments (role & index)."
-
 
 extendedPrivateKeyToCardanoCliPrivateKey = error "Use cardano-cli's readSomeCardanoAddressSigningKeyFile"
--- adawallet/src/Transaction.hs
+++ adawallet/src/Transaction.hs
@@ -2,27 +2,26 @@
 
 module Transaction where
 
-import qualified Blockfrost.Client   as BF
-import           Cardano.Api         as Api
-import           Cardano.Api.Shelley
-import qualified Data.ByteString     as BS
-import qualified Data.Foldable       as Fld
-import qualified Data.Map.Strict     as Map
-import           Data.String         (IsString (..))
-import           Data.Text           (Text)
-import qualified Data.Text           as T
-import qualified Data.Text           as Text
-import qualified Data.Text.Encoding  as TE
-import qualified Data.Text.Encoding  as Text
-import           Money
-import           Numeric             (readHex)
-import           Prelude
-import           Text.Pretty.Simple
-import GHC.Stack
-import Data.Monoid
-import Data.Maybe (fromMaybe)
+import qualified Blockfrost.Client as BF
+import Cardano.Api as Api
+import Cardano.Api.Shelley
 import Data.Bifunctor
-
+import qualified Data.ByteString as BS
+import qualified Data.Foldable as Fld
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromMaybe)
+import Data.Monoid
+import Data.String (IsString (..))
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.Encoding as Text
+import GHC.Stack
+import Money
+import Numeric (readHex)
+import Text.Pretty.Simple
+import Prelude
 
 readBfTx :: HasCallStack => IO ()
 readBfTx = do
@@ -32,7 +31,7 @@ readBfTx = do
   let address = "addr_test1wqh4yha0ndhwykrh9cuhr47nh2y97zvkls74h4jq6uhlpacujv3z3"
       -- TODO figure this out dynamically
       era = AlonzoEraOnwardsBabbage
-  (latestBlocks,  utxos) <- fmap (either (error . show) id) . BF.runBlockfrost prj $ do
+  (latestBlocks, utxos) <- fmap (either (error . show) id) . BF.runBlockfrost prj $ do
     latestBlocks <- BF.getLatestBlock
     utxos <- BF.getAddressUtxos address
     pure (latestBlocks, utxos)
@@ -42,46 +41,53 @@ readBfTx = do
   let apiTxs = toApiTxOut era address <$> utxos
   pPrint apiTxs
 
-toApiUTxO
-  :: HasCallStack
-  => AlonzoEraOnwards era
-  -> BF.Address
-  -> [BF.AddressUtxo]
-  -> UTxO era
+toApiUTxO ::
+  HasCallStack =>
+  AlonzoEraOnwards era ->
+  BF.Address ->
+  [BF.AddressUtxo] ->
+  UTxO era
 toApiUTxO w addr =
   Fld.foldl'
-    (\(UTxO acc) addrUTxO -> UTxO $ acc <> Map.singleton (toApiTxIn addrUTxO) (toApiTxOut w addr addrUTxO))
+    ( \(UTxO acc) addrUTxO -> UTxO $ acc <> Map.singleton (toApiTxIn addrUTxO) (toApiTxOut w addr addrUTxO)
+    )
     (UTxO mempty)
 
 toApiTxIn :: BF.AddressUtxo -> TxIn
 toApiTxIn adUTxO =
-  TxIn (fromString . Text.unpack . BF.unTxHash $ BF._addressUtxoTxHash adUTxO) (TxIx . fromIntegral $ BF._addressUtxoOutputIndex adUTxO)
+  TxIn
+    (fromString . Text.unpack . BF.unTxHash $ BF._addressUtxoTxHash adUTxO)
+    (TxIx . fromIntegral $ BF._addressUtxoOutputIndex adUTxO)
 
-toApiTxOut
-  :: forall era. HasCallStack
-  => AlonzoEraOnwards era
-  -> BF.Address
-  -> BF.AddressUtxo
-  -> TxOut CtxUTxO era
+toApiTxOut ::
+  forall era.
+  HasCallStack =>
+  AlonzoEraOnwards era ->
+  BF.Address ->
+  BF.AddressUtxo ->
+  TxOut CtxUTxO era
 toApiTxOut w (BF.Address addr) addrUTxO =
   case deserialiseAddress (AsAddress AsShelleyAddr) addr of
     Nothing -> error $ "toApiTxOut: Shelley address deserialization failed: " <> Text.unpack addr
     Just bech32Addr ->
       let addrInEra = AddressInEra (ShelleyAddressInEra (alonzoEraOnwardsToShelleyBasedEra w)) bech32Addr
-          val = toLedgerValue (alonzoEraOnwardsToMaryEraOnwards w) $ Fld.foldl' (\acc amt -> acc <> toApiValue amt) mempty $ BF._addressUtxoAmount addrUTxO
+          val =
+            toLedgerValue (alonzoEraOnwardsToMaryEraOnwards w) $
+              Fld.foldl' (\acc amt -> acc <> toApiValue amt) mempty $
+                BF._addressUtxoAmount addrUTxO
           mDatHash = BF._addressUtxoDataHash addrUTxO
           mInlineDat = BF._addressUtxoInlineDatum addrUTxO
           sbe = alonzoEraOnwardsToShelleyBasedEra w
           v = shelleyBasedEraConstraints sbe $ TxOutValueShelleyBased sbe val :: TxOutValue era
-      -- NB: Blockfrost only returns the reference script hash
-      in TxOut addrInEra v (toApiDatum w mDatHash mInlineDat) ReferenceScriptNone
+       in -- NB: Blockfrost only returns the reference script hash
+          TxOut addrInEra v (toApiDatum w mDatHash mInlineDat) ReferenceScriptNone
 
-toApiDatum
-  :: HasCallStack
-  => AlonzoEraOnwards era
-  -> Maybe BF.DatumHash
-  -> Maybe BF.InlineDatum
-  -> TxOutDatum CtxUTxO era
+toApiDatum ::
+  HasCallStack =>
+  AlonzoEraOnwards era ->
+  Maybe BF.DatumHash ->
+  Maybe BF.InlineDatum ->
+  TxOutDatum CtxUTxO era
 toApiDatum w mDatumHash mInlineDatum = do
   let era = alonzoEraOnwardsToCardanoEra w
   case (mDatumHash, mInlineDatum) of
@@ -90,15 +96,16 @@ toApiDatum w mDatumHash mInlineDatum = do
       TxOutDatumHash w $ fromString $ Text.unpack dHash
     (_, Just (BF.InlineDatum (BF.ScriptDatumCBOR sCbor))) -> either error id $ do
       let bytes = hexToByteString sCbor
-      sData <- first (("toApiDatum: Error deserializing inline datum: " <>) . show) $
-        deserialiseFromCBOR AsScriptData bytes
+      sData <-
+        first (("toApiDatum: Error deserializing inline datum: " <>) . show) $
+          deserialiseFromCBOR AsScriptData bytes
       let hScriptData = unsafeHashableScriptData sData
           hash = hashScriptDataBytes hScriptData
       maybe (Left $ "Impossible case - unsupported era: " <> show (pretty era)) pure
         . getFirst
         . mconcat
-        . map First $
-          [ forEraInEonMaybe era $ \w ->
+        . map First
+        $ [ forEraInEonMaybe era $ \w ->
               TxOutDatumInline w hScriptData
           , forEraInEonMaybe era $ \w ->
               TxOutDatumHash w hash
@@ -106,7 +113,7 @@ toApiDatum w mDatumHash mInlineDatum = do
           ]
 
 toApiValue :: HasCallStack => BF.Amount -> Api.Value
-toApiValue (BF.AdaAmount ll)  = lovelaceToValue . fromInteger $ toInteger ll
+toApiValue (BF.AdaAmount ll) = lovelaceToValue . fromInteger $ toInteger ll
 toApiValue (BF.AssetAmount a) = toApiMultiAsset a
 
 hexToByteString :: HasCallStack => T.Text -> BS.ByteString
@@ -122,6 +129,9 @@ toApiMultiAsset sDiscrete =
           (error $ "toApiMultiAsset: " <> Text.unpack aName <> " is not hex encoded")
           id
           (deserialiseFromRawBytesHex AsAssetName $ Text.encodeUtf8 aName)
-  in valueFromList [ (AssetId (fromString $ Text.unpack pId) assetName
-                   , fromInteger q)
-                   ]
+   in valueFromList
+        [
+          ( AssetId (fromString $ Text.unpack pId) assetName
+          , fromInteger q
+          )
+        ]
