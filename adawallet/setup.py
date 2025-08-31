fda1e4f85ddfda6db2e228e74f882dd34901d8ee
John Lotoski <john.lotoski@iohk.io>
2025-08-31 01:50:05 -0500

    imp: integrate adawallet nix pkg, docopt, subcmd to __version__

--- adawallet/setup.py
+++ adawallet/setup.py
@@ -1,8 +1,18 @@
+import re
+from pathlib import Path
 from distutils.core import setup
+
+def get_version():
+    text = Path("adawallet/__init__.py").read_text()
+    match = re.search(r'^__version__ = "(.+)"', text, re.M)
+    if not match:
+        raise Exception("Unable to find __version__ in name/__init__.py")
+    return match.group(1)
+
 
 setup(
     name='adawallet',
-    version='0.0.0',
+    version=get_version(),
     scripts=['bins/adawallet'],
     packages=['adawallet']
 )
